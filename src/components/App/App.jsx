30b5ee36de91fd271c326cd8bf477fd21fd9dd22
Oleksii Korolenko <as_vostok@i.ua>
2023-04-03 20:08:03 +0300

    add correct

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,10 +9,10 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    { id: 'id-1', name: 'Adam Sandler', number: '459-12-56' },
+    { id: 'id-2', name: 'Jennifer Aniston', number: '443-89-12' },
+    { id: 'id-3', name: 'Brad Pitt', number: '645-17-79' },
+    { id: 'id-4', name: 'Angelina Jolie', number: '227-91-26' },
   ]);
 
   const [filter, setFilter] = useState('');
